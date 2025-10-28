47e9cf6d378a92d29bda2a05f52c8ef86f9e6ca5
Naveed Malik <naveedmalik1503@gmail.com>
2025-10-28 18:58:40 +0530

    updated

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,14 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["avatars.githubusercontent.com"],
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ✅ Required for static export on Render
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
